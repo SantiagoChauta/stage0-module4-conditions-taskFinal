913c45a488216620c97a46cfc54b30da6c99f05d
Santiago Chauta <santiagochauta@gmail.com>
2023-06-07 20:50:22 -0500

    finish

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -2,6 +2,18 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class DaysInMonth {
     public void printDays(int year, int month) {
-
+    	if (year<1 || month<1 || month>12) {
+    		System.out.println("invalid date");
+    	}
+    	else if(month==2) {
+    		System.out.println(year%4==0 && (year%400==0 || year%100!=0)? 29:28);
+    	}
+    	else if((month%2!=0 && month<8) || (month>=8 &&month%2==0)) {
+    		System.out.println(31);
+    	}else if((month%2==0 && month<8) || (month>=8 &&month%2!=0)) {
+    		System.out.println(30);
+    	}else {
+    		System.out.println();
+    	}
     }
 }
--- src/main/java/school/mjc/stage0/conditions/finalTask/TriangleSidesValidator.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/TriangleSidesValidator.java
@@ -2,6 +2,13 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class TriangleSidesValidator {
     public void validate(double firstSide, double secondSide, double thirdSide) {
-
+    	String ans = firstSide+secondSide>thirdSide 
+    			? secondSide + thirdSide > firstSide
+    					? thirdSide+firstSide>secondSide
+    							? "this is a valid triangle"
+    							: "it's not a triangle"
+    					: "it's not a triangle"
+    			: "it's not a triangle";
+    	System.out.println(ans);
     }
 }
